590700d57b417c55d6e56f32f232e3281aa9cc55
Zsuzsanna <lengyel.zsuzsanna@gmail.com>
2025-01-30 13:49:55 +0100

    Add project files.

---
+++ DomainRecord.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+public class DomainRecord
+{
+    public string DomainNev { get; private set; }
+    public string IpCim { get; private set; }
+
+    // Konstruktor, amely egy fájlból olvasott sort kap paraméterként
+    public DomainRecord(string sor)
+    {
+        var adatok = sor.Split(';');
+        if (adatok.Length == 2)
+        {
+            DomainNev = adatok[0];
+            IpCim = adatok[1];
+        }
+        else
+        {
+            throw new ArgumentException("Hibás sorformátum: " + sor);
+        }
+    }
+
+    
+    public string GetDomainSzint(int szint)
+    {
+        return DomainSzint(this.DomainNev, szint);
+    }
+        
+    public static string DomainSzint(string domainnev, int szint)
+    {
+        var reszek = domainnev.Split('.');
+        if (szint >= 1 && szint <= reszek.Length)
+        {
+            return string.Join(".", reszek.Skip(reszek.Length - szint));
+        }
+        else
+        {
+            return "nincs";
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{DomainNev} - {IpCim}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<DomainRecord> domainLista = new List<DomainRecord>();
+
+        try
+        {
+            foreach (var sor in File.ReadAllLines("csudh.txt"))
+            {
+                if (!sor.StartsWith("domain-name"))  // Az első sor fejléc, azt kihagyjuk
+                {
+                    domainLista.Add(new DomainRecord(sor));
+                }
+            }
+
+            // Adatok kiírása
+            Console.WriteLine("Beolvasott domain-IP párosok:");
+            foreach (var record in domainLista)
+            {
+                Console.WriteLine(record);
+            }
+
+            Console.WriteLine($"\nÖsszesen {domainLista.Count} cím van a listában.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Hiba történt: " + ex.Message);
+        }
+    }
+
+    public static string DomainSzint(string domainnev, int szint)
+    {
+        var reszek = domainnev.Split('.');
+        if (szint > 0 && szint <= reszek.Length)
+        {
+            return string.Join(".", reszek.Skip(reszek.Length - szint));
+        }
+        else
+        {
+            throw new ArgumentException("Érvénytelen szint érték: " + szint);
+        }
+    }
+
+
+}
